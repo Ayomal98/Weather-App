1309a2fdee504c0e55889183abc6a2a364ad1cdc
Ayomal Praveen <ayomalpraveen33@gmail.com>
2020-10-30 11:15:00 +0530

    added the Dark-sky APi & other stylings

---
+++ app.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let tempDescription = document.querySelector(".temprature-description");
+  let tempDegree = document.querySelector(".temprature-degree");
+  let locationTimeZone = document.querySelector(".location-timezone");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+      fetch(api)
+        .then((data) => {
+          return data.json();
+        })
+        .then((response) => {
+          console.log(response);
+          const { apparentTemperature, summary } = response.currently;
+          tempDegree.textContent = apparentTemperature;
+          tempDescription.textContent = summary;
+          locationTimeZone.textContent = response.timezone;
+        });
+    });
+  }
+});
